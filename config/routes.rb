a650a2f3d6faa4dddd7bfc945c5277915cd679e1
Lorin Werthen <donvittoriomusic@gmail.com>
2015-07-14 22:37:06 +0200

    Added users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  resources :users
+  #get 'users' => 'users#index'
+  #get 'users/:id' => 'users#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create name: 'Don'
+User.create name: 'Iepoev'
+User.create name: 'Silox'
+User.create name: 'Julien'
