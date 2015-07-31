88cb9f36d53be6ce3ee582f44e68e8d2a1211d37
Lorin Werthen <donvittoriomusic@gmail.com>
2015-07-31 23:32:22 +0200

    made a better new group page and improved the games page somewhat

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -35,6 +35,6 @@ class GroupsController < ApplicationController
   end
 
   def group_params
-    params.require(:group).permit(:game_id, :user_ids)
+    params.require(:group).permit(:game_id, user_ids: [])
   end
 end
