3681b8e098758f075eb958190d0cca0d6a6b7498
Lorin Werthen <donvittoriomusic@gmail.com>
2015-09-01 01:27:42 +0200

    notification scheduling in after_save callback

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -8,25 +8,11 @@ class NotificationsController < ApplicationController
     notification = Notification.new(notification_params)
     notification.save!
 
-    schedule_notification(notification)
-
     redirect_to action: 'index'
   end
 
   private
   def notification_params
     params.require(:notification).permit(:content, :scheduled_at)
-  end
-
-  def schedule_notification(notification)
-    s = Rufus::Scheduler.singleton
-
-    s.jobs(tag: notification.id).each do |job|
-      job.unschedule
-    end
-
-    s.at notification.scheduled_at, tag: notification.id do
-      WebsocketRails[:all].trigger :message, params[:notification][:content]
-    end
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -9,4 +9,19 @@
 #
 
 class Notification < ActiveRecord::Base
+  after_save :schedule_notification
+
+  private
+
+  def schedule_notification
+    s = Rufus::Scheduler.singleton
+
+    s.jobs(tag: self.id).each do |job|
+      job.unschedule
+    end
+
+    s.at self.scheduled_at, tag: self.id do
+      WebsocketRails[:all].trigger :message, self.content
+    end
+  end
 end
