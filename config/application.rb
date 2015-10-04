13f727e36ea5bd665309888722bf983485a90555
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-04 23:34:35 +0200

    set up reactify

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Lana
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # reactify javascripts
+    config.browserify_rails.commandline_options = "-t reactify --extension=\".js.jsx\""
   end
 end
