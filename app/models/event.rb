9d48eb0713829637aa8d6bf15bcb0d53b22cd69d
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-07 15:02:46 +0200

    bundle exec annotate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  description :text
+#  moment      :datetime         not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Event < ActiveRecord::Base
 
 end
--- test/models/chat_message_test.rb
+++ test/models/chat_message_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: chat_messages
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ChatMessageTest < ActiveSupport::TestCase
