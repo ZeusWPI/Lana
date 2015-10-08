1e7d01030d5a3a201dade1e63c3148acc81476ef
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-08 16:38:50 +0200

    add redux-thunk

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,18 +1,18 @@
 import Immutable from 'immutable';
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducer';
 import App from './components/app';
 
 const initialState = {
-  messages: Immutable.List(),
   events: Immutable.List([
     {name: 'hallo', date: Date.now()+1000},
     {name: 'hai', date: Date.now()+2000}
   ])
 };
 
-let store = createStore(reducer, initialState);
+let store = applyMiddleware(thunk)(createStore)(reducer, initialState);
 
 $(document).ready(function(){
   React.render(
