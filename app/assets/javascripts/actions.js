72d94db0127969a8811a4942191e7be73a36dbd6
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-13 17:34:48 +0200

    downcase action names

--- app/assets/javascripts/actions.js
+++ app/assets/javascripts/actions.js
@@ -2,10 +2,10 @@ import Immutable from 'immutable';
 import fetch from 'isomorphic-fetch';
 import { createAction } from 'redux-actions';
 // messages
-export const addMessage = createAction('ADD_MESSAGE');
+export const addMessage = createAction('add_message');
 
 // events
-export const receiveEvents = createAction('RECEIVE_EVENTS');
+export const receiveEvents = createAction('receive_events');
 
 export function fetchEvents(){
   return dispatch =>
--- app/assets/javascripts/reducer.js
+++ app/assets/javascripts/reducer.js
@@ -4,13 +4,13 @@ import Immutable from 'immutable';
 var Actions = require('./actions');
 
 const messages = handleActions({
-  ADD_MESSAGE: (state, action) => (
+  add_message: (state, action) => (
     state.push(action.payload)
   )
 }, Immutable.List());
 
 const events = handleActions({
-  RECEIVE_EVENTS: (state, action) => (
+  receive_events: (state, action) => (
       Immutable.List(action.payload)
   )
 }, Immutable.List());
