b7af1021825f429b4d8834531ed1f5bcd8e6969d
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-13 23:47:10 +0200

    refactor actions

--- app/assets/javascripts/actions/events.js
+++ app/assets/javascripts/actions/events.js
@@ -1,9 +1,4 @@
-import Immutable from 'immutable';
-import fetch from 'isomorphic-fetch';
 import { createAction } from 'redux-actions';
-// messages
-export const addMessage = createAction('add_message');
 
-// events
 export const addEvent = createAction('add_event');
 export const receiveEvents = createAction('receive_events');
---
+++ app/assets/javascripts/actions/messages.js
@@ -0,0 +1,3 @@
+import { createAction } from 'redux-actions';
+
+export const addMessage = createAction('add_message');
