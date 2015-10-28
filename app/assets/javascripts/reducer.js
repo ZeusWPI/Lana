673a5867502119d81920c11792dea56b9dcb3f0c
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-28 22:18:41 +0100

    normalized events and timeline

--- app/assets/javascripts/reducer.js
+++ app/assets/javascripts/reducer.js
@@ -9,15 +9,6 @@ const messages = handleActions({
   )
 }, Immutable.List());
 
-const events = handleActions({
-  add_event: (state, action) => (
-    state.push(action.payload)
-  ),
-  receive_events: (state, action) => (
-      Immutable.List(action.payload)
-  )
-}, Immutable.List());
-
 const groups = handleActions({
   receive_groups: (state, action) => (
     Immutable.List(action.payload)
@@ -28,9 +19,34 @@ const current_user = handleActions({
   login: (state, action) => action.payload
 }, null);
 
+
+
+const events = handleActions({
+  receive_events: (state, action) =>
+    (action.payload.reduce(
+      (map, event) => map.set(event.id, event),
+      Immutable.Map())
+    ),
+  add_event: (state, action) =>
+    (state.set(action.payload.id, action.payload))
+}, Immutable.Map());
+
+const data = combineReducers({
+  events
+});
+
+const timeline = handleActions({
+  receive_events: (state, action) =>
+    Immutable.List(action.payload.map(e => (e.id)))
+      .sortBy(e => e.moment),
+  add_event: (state, action) =>
+    (state.push(action.payload.id).sortBy(e => e.moment))
+}, Immutable.List());
+
 export default combineReducers({
+  data,
+  timeline,
   messages,
-  events,
   groups,
   current_user,
   router
