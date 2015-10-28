5b53f9ef88d99150d051b3d019bdea718b2aa02b
Rien Maertens <maertensrien@gmail.com>
2015-10-28 01:17:58 +0100

    Very basic implementation of a game list

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/app';
-import Timeline from './containers/timeline';
+import GameList from './containers/games';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={Timeline}/>
+    <IndexRoute component={GameList}/>
   </Route>
 );
