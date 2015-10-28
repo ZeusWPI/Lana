a8dd6d3a3516dd0b17f5bcca136eeb7dde2c3ce2
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-28 18:54:15 +0100

    hide event form by default

--- app/assets/javascripts/components/timeline/event_form.js
+++ app/assets/javascripts/components/timeline/event_form.js
@@ -5,7 +5,7 @@ export default class EventForm extends Component {
   constructor(props, context) {
     super(props, context);
     if (this.props.event === undefined) {
-      this.state = {};
+      this.state = { moment: moment().add(30, 'minutes')};
     } else {
       this.state = this.props.event;
     }
@@ -27,6 +27,7 @@ export default class EventForm extends Component {
               <DateTimeField
                 dateTime={this.state.moment}
                 onChange={ e => this.setState({moment: new Date(- -e)}) }
+                minDate={moment()}
               />
             </div>
           </div>
