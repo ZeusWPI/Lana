4cd8e33d0b3e71bba1111d00d16a4078048331d9
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-29 16:39:32 +0100

    user page stub

---
+++ app/assets/javascripts/components/user.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class User extends Component {
+  render() {
+    return (
+      <div>
+        Hoi
+      </div>
+    );
+  }
+}
+
+export default User;
---
+++ app/assets/javascripts/containers/user.js
@@ -0,0 +1,8 @@
+import { connect } from 'react-redux';
+import User from '../components/user';
+
+function props(state) {
+  return {};
+}
+
+export default connect(props)(User);
