364e859a1447d875a643031a095ffee1716740cb
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-29 22:06:28 +0100

    put LFG on the game page

--- app/assets/javascripts/components/game.js
+++ app/assets/javascripts/components/game.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Timeline from './timeline';
+import Groups from './groups';
 
 class Game extends Component {
   render(){
+    const { events, groups, eventActions, groupActions } = this.props;
     return (
       <div>
         <Timeline
-          events={this.props.events}
-          eventActions={this.props.eventActions}
+          events={events}
+          eventActions={eventActions}
         />
+        <Groups groups={groups} actions={groupActions}/>
       </div>
     );
   }
--- app/assets/javascripts/containers/game.js
+++ app/assets/javascripts/containers/game.js
@@ -1,14 +1,23 @@
 import { connect } from 'react-redux';
 import Game from '../components/game';
 import { addEvent } from '../actions/events';
+import { addGroup, joinGroup, leaveGroup } from '../actions/groups.js';
+
 
 function props(state){
-  const { data } = state;
+  const { current_user, data } = state;
+
+  const username = (user_id) => data.users.getIn([user_id, 'name'])
+  const groups = data.groups.map(
+    group => group.set('joined', group.get('members').includes(current_user.id))
+                  .update('members', ms => ms.map(username))
+  ).toIndexedSeq().toJS();
+
   return {
     title: 'Geweren en Explosies 24',
     image_url: 'http://fotodes.ru/upload/img1342258123.jpg',
-    groups: data.groups.toList().toJS(),
-    events: data.events.toList().toJS()
+    events: data.events.toList().toJS(),
+    groups
   };
 }
 
@@ -16,6 +25,11 @@ function actions(dispatch){
   return {
     eventActions: {
       add: e => dispatch(addEvent(e))
+    },
+    groupActions: {
+      add: e => dispatch(addGroup(e)),
+      join: e => dispatch(joinGroup(e)),
+      leave: e => dispatch(leaveGroup(e))
     }
   };
 }
