a8d61ee2c6d78d38748eb5e70062e47e40fb8bfb
Ilion Beyst <ilion.beyst@gmail.com>
2015-10-30 07:57:33 +0100

    no more calendar dates

--- app/assets/javascripts/components/timeline/event.js
+++ app/assets/javascripts/components/timeline/event.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import DateTimeField from 'react-bootstrap-datetimepicker';
 
 class Event extends Component {
+  formatMoment(){
+    const time = moment(this.props.moment);
+    if (time < moment().add(1, 'day')){
+      return time.format('HH:MM');
+    } else {
+      return moment.calendar();
+    }
+  }
   render() {
     const { name, description } = this.props;
-    const time=moment(this.props.moment).calendar();
     return (
       <div className='event new panel panel-default'>
         <div className='panel-body'>
           <div className='header'>
             <div className='name'>{name}</div>
-            <div className='time'>{time}</div>
+            <div className='time'>{this.formatMoment()}</div>
           </div>
           <div className='description'>{description}</div>
         </div>
