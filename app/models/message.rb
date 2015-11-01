f13a06b7c0ec3f4d8bb83c6d8e5b383e7797f3cc
Lorin Werthen <Lorin.WerthenBrabants@UGent.be>
2015-11-01 23:56:55 +0100

    Don't allow blank chat messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,9 +2,11 @@ class Message < ActiveRecord::Base
   extend Broadcastable
   belongs_to :user
   belongs_to :group
+  validates :contents, presence: true
 
   def as_json(options)
     self.attributes.slice(
       'id', 'contents', 'user_id', 'group_id')
   end
+
 end
